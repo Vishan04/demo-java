a099b81e6c9a868fb3ac26bd9a6083483b1c4a41
vishan sree <sreevishan29@gmail.com>
2026-05-28 18:10:57 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
 
         int a = 10;
-        int b = 20;
+        int b = 10;
         int sum = a + b;
 
         System.out.println("Sum = " + sum);
